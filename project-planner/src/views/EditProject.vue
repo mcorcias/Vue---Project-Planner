c09680071687a57e90f5fc86bddf84655a275a76
mcorcias <mosescorcias@gmail.com>
2021-01-29 12:44:05 +0200

    This project - 'Project Planner' is a small project for learning all the basic vue js before we use in composition API

---
+++ project-planner/src/views/EditProject.vue
@@ -0,0 +1,45 @@
+<template>
+   <form @submit.prevent="handleUpdate">
+      <label>Title</label>
+      <input type="text" v-model="title" required>
+      <label>Details:</label>
+      <textarea v-model="details" required></textarea>
+      <button>Update Project</button>
+  </form>
+</template>
+
+<script>
+export default {
+    props:['id'],
+    data(){
+        return{
+            title:'',
+            details:'',
+            url:`http://localhost:3000/projects/${this.id}`
+        }
+    },
+    methods:{
+        handleUpdate(){
+            fetch(this.url,{
+                method: 'PATCH',
+                headers:{'Content-Type':'application/json'},
+                body: JSON.stringify({title:this.title,details:this.details})
+            }).then(()=>{
+                 this.$router.push('/');
+            }).catch(err=>console.log(err.message))
+        }
+    },
+    mounted(){
+        fetch(this.url)
+        .then(res=>res.json())
+        .then(data=>{
+            this.title=data.title
+            this.details=data.details
+        })
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ project-planner/src/views/Home.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="home">
+    <FilterNav @filterChange="current=$event" :current="current"/>
+    <div v-if="projects.length">
+      <div v-for="project in filteredProjects" :key="project.id">
+        <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import SingleProject from '../components/SingleProject.vue'
+import FilterNav from '../components/FilterNav.vue'
+
+export default {
+  name: 'Home',
+  components: {SingleProject,FilterNav},
+  data(){
+    return{
+      projects:[],
+      current: 'all'
+    }
+  },
+  methods:{
+    handleDelete(id){
+      this.projects=this.projects.filter(project=>project.id!=id)
+    },
+    handleComplete(id){
+      this.projects.forEach((project)=>{
+        if(project.id==id){
+          project.complete=!project.complete
+          return
+        }
+      })
+    }
+  },
+  computed:{
+    filteredProjects(){
+      if(this.current==='completed'){
+        return this.projects.filter(project=>project.complete)
+      }
+      else if(this.current==='ongoing'){
+        return this.projects.filter(project=>!project.complete)
+      } 
+      return this.projects
+    }
+  },
+  mounted(){
+    fetch('http://localhost:3000/projects')
+    .then(res=>res.json())
+    .then(data=>this.projects=data)
+    .catch(err=>console.log(err.message))
+  }
+}
+</script>
